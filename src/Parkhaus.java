0f7022b7da3493bac50fe49b96ab5c3177c29de0
Noah Jutz <noah@noahjutz.com>
2025-10-16 16:34:36 +0200

    Move park/unpark to Parkhaus, move Auto.run to Simulation

--- src/Parkhaus.java
+++ src/Parkhaus.java
@@ -5,18 +5,27 @@ public class Parkhaus {
     final static int CAPACITY = 10;
     private Queue<Auto> autos = new LinkedList<>();
 
-    void park(Auto auto) {
-        assert getLoad() < CAPACITY;
+    synchronized void park(Auto auto) {
+        while (autos.size() >= CAPACITY) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
         autos.add(auto);
+        notifyAll();
     }
 
-    void unpark() {
+    synchronized void unpark(Auto auto) {
         assert getLoad() > 0;
+        while (autos.peek() != auto) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
         autos.remove();
-    }
-
-    Auto get() {
-        return autos.peek();
+        notifyAll();
     }
 
     int getLoad() {
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,21 +1,25 @@
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Simulation {
     final static int N_AUTOS = 20;
+    private final Random random = new Random();
     private final Parkhaus p = new Parkhaus();
-    private final List<Auto> autos = IntStream.range(0, N_AUTOS).mapToObj(i -> {
-                var a = new Auto(p, i);
-                a.setDaemon(true);
-                return a;
-            })
-            .collect(Collectors.toList());
-
+    private final List<Auto> autos = IntStream.range(0, N_AUTOS).mapToObj(Auto::new).toList();
 
     void start() {
         for (var auto : autos) {
-            auto.start();
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        Thread.sleep(random.nextInt(10 * 1000));
+                    } catch (InterruptedException e) {
+                    }
+                    p.park(auto);
+                    p.unpark(auto);
+                }
+            }).start();
         }
 
         try {
