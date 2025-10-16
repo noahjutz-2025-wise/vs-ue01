ea25016a23ebdac73a123d28a0d7cd81ef9e9708
Noah Jutz <noah@noahjutz.com>
2025-10-16 14:20:16 +0200

    Implement Parkhaus

--- src/Parkhaus.java
+++ src/Parkhaus.java
@@ -1,2 +1,18 @@
 public class Parkhaus {
+    private final int CAPACITY = 10;
+    private int load = 0;
+
+    void inc() {
+        assert load < CAPACITY;
+        load++;
+    }
+
+    void dec() {
+        assert load > 0;
+        load--;
+    }
+
+    int getLoad() {
+        return load;
+    }
 }
